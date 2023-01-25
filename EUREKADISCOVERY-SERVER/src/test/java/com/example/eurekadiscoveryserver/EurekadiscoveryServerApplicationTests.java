a62fb4c14a7e4fc881a5ac733be3d140d971514e
unknown <oussama-nassoh>
2023-01-25 22:25:20 +0100

    my projet

---
+++ EUREKADISCOVERY-SERVER/src/test/java/com/example/eurekadiscoveryserver/EurekadiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
